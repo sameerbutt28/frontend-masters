c915f53ff20f7db3a84316fdd2be2b32ea5d5536
sameerbutt28 <sameerofficial929@gmail.com>
2023-12-12 16:53:30 +0500

    introducing useState in searhParams

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,15 +1,20 @@
-
+import { useState } from "react";
 const SearchParams = () => {
-  const location = "Islamabad, Pakistan ";
+  const [location, setLocation] = useState("");
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">Location</label>
-        <input id="location" value= {location} placeholder="location" />
+        <input
+          onChange={(e) => setLocation(e.target.value)}
+          id="location"
+          value={location}
+          placeholder="location"
+        />
         <button>Submit</button>
       </form>
-    </div> 
+    </div>
   );
 };
-//everytime an event happens the react render everything on the page 
+//everytime an event happens the react render everything on the page from top to bottom
 export default SearchParams;
