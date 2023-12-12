da9ac27eed17e678b895ea456406fde711603c90
sameerbutt28 <sameerofficial929@gmail.com>
2023-12-12 14:17:31 +0500

    bs

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Pet from "./Pet";
 import  { createRoot }  from "react-dom";
 
   
-  const App = () => { 
+  const App = () => {   
   return (
-    <div>
+    <div> 
       <h1>Adopt Me!</h1>
       <Pet name="Luna" animal="dog" breed="Havanese" />
       <Pet name="Pepper" animal="bird" breed="Cockatiel" />
