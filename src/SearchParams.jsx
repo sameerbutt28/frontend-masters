57e2230858d6524e75cdf78c75b92f2863e7b75f
sameerbutt28 <sameerofficial929@gmail.com>
2023-12-14 23:46:36 +0500

    refactoring code

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,20 +1,54 @@
 import { useState } from "react";
+const ANIMALS = ["cat", "dog", "horse", "eligator", "dinosour"];
+const BREEDS = ["poodle"];
 // let counter= 0;
 const SearchParams = () => {
-    // counter++
+  // counter++
   const [location, setLocation] = useState("Gujar khan");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
   return (
     //everytime we clicking on it renders everything again and again and we can seee through the counter
     <div className="search-params">
-        {/* <h2>{counter}</h2> */}
+      {/* <h2>{counter}</h2> */}
       <form>
         <label htmlFor="location">Location</label>
         <input
-          onChange={(e) => setLocation(e.target.value)}
+          onChange={(e) => setLocation(e.target.value)
+        }
           id="location"
           value={location}
           placeholder="location"
         />
+        <label htmlFor="animal">Animal</label>
+        <select
+          id="animal"
+          value={animal}
+          onChange={(e) =>{
+            setAnimal(e.target.value)
+                  setBreed("");
+          } 
+
+        }
+        >
+          <option />
+          {ANIMALS.map((animal) => {
+            return <option key={animal}>{animal}</option>;
+          })}
+        </select>
+        <label htmlFor="breed">Breed</label>
+        <select
+          id="breed"
+          disabled = {BREEDS.length === 0}
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+        >
+          <option />
+          {BREEDS.map((breed) => {
+            return <option key={breed}>{breed}</option>;
+          })}
+        </select>
+
         <button>Submit</button>
       </form>
     </div>
