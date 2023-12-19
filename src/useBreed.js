0fe4b965ce783120c8659a6d125bd6e8628f524b
sameerbutt28 <sameerofficial929@gmail.com>
2023-12-19 23:59:25 +0500

    bs

--- src/useBreed.js
+++ src/useBreed.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 const localCache= {};
 export default function useBreed(animal) 
-{     
+{       
     const [breedList, setBreedList] = useState([]);
     const [status, setStatus] = useState("unloaded");
     useEffect(()=>
