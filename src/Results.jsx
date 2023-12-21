f2d1ac804e3cb756d6e588f12528df9b15392cef
sameerbutt28 <sameerofficial929@gmail.com>
2023-12-21 18:19:38 +0500

    adding comment about pets props

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,7 +1,7 @@
 import Pet from "./Pet"
-const Results = () => {
+const Results = ({pets}) => { //here the pet in jsx tells us that we are expecting a props named as pets on the other component where we want to call it.
   return (
-    <div>
+    <div className="search">
       
     </div>
   )
