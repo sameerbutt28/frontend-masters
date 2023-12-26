98b588126226bef9cff57b72159ab910c21e7b64
sameerbutt28 <sameerofficial929@gmail.com>
2023-12-26 19:23:45 +0500

    bs

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,7 +1,7 @@
 import Pet from "./Pet";
 const Results = ({ pets }) => {
   //here the pet in jsx tells us that we are expecting a props named as pets on the other component where we want to call it.
-  return (          
+  return (           
     <div className="search">
       {!pets.length ? (
         <h1>No Pets found.</h1>
