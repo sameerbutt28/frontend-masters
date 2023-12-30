482c3a7df96524694646cd3b6fdd865eb7172243
sameerbutt28 <sameerofficial929@gmail.com>
2023-12-31 02:35:41 +0500

    refactoring app.js

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,77 +1,93 @@
 import { useState, useEffect } from "react";
-import useBreedList from "./useBreed";
-import Results from "./Results";
+import useBreedList from "./useBreed"; // Ensur this path is correct
+import Results from "./Results"; // Ensure this path is correct
+
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
-// let counter= 0;
+
 const SearchParams = () => {
-  // counter++
-  const [location, setLocation] = useState("Gujar khan");
+  const [location, setLocation] = useState("Seattle, WA"); // Default location
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const BREEDS = useBreedList(animal);
+  const [error, setError] = useState(null);
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
-  }, [animal]); // the term in the use effect array shows whenever the animal changes the useeffect will render the functon like everytime the animal option changes the requestPet() will be rendered.
-  const requestPets = async () => {
-    const res = await fetch(
-      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-    );
-    const json = await res.json();
-    setPets(json.pets);
-  };
+  }, [animal, location, breed]); // Fetch pets whenever these states change
+
+  async function requestPets() {
+    try {
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+      );
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      const json = await res.json();
+      setPets(json.pets);
+    } catch (e) {
+      setError(e.message);
+    }
+  }
+
   return (
-    //everytime we clicking on it renders everything again and again and we can seee through the counter
     <div className="search-params">
-      {/* <h2>{counter}</h2> */}
+      {error && <h2>Error: {error}</h2>}
       <form
         onSubmit={(e) => {
           e.preventDefault();
           requestPets();
         }}
       >
-        <label htmlFor="location">Location</label>
-        <input
-          onChange={(e) => setLocation(e.target.value)}
-          id="location"
-          value={location}
-          placeholder="location"
-        />
-        <label htmlFor="animal">Animal</label>
-        <select
-          id="animal"
-          value={animal}
-          onChange={(e) => {
-            setAnimal(e.target.value);
-            setBreed(""); //this set breed shows that wheneever onchage happens in the animal then the breed will be set to empty string
-          }}
-        >
-          <option />
-          {/* here the blank option shows that you can select the black option from th option bar */}
-          {ANIMALS.map((animal) => {
-            return <option key={animal}>{animal}</option>;
-          })}
-        </select>
-        <label htmlFor="breed">Breed</label>
-        <select
-          id="breed"
-          //if length === zero if there is no item in the array then the option willl be disabled
-          disabled={BREEDS.length === 0}
-          value={breed}
-          onChange={(e) => setBreed(e.target.value)}
-        >
-          <option />
-          {BREEDS.map((breed) => {
-            return <option key={breed}>{breed}</option>;
-          })}
-        </select>
-
-        <button>Submit</button>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button type="submit">Submit</button>
       </form>
       <Results pets={pets} />
     </div>
   );
 };
-//everytime an event happens the react render everything on the page from top to bottom
+
 export default SearchParams;
