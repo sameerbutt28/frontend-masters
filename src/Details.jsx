931426e5b662b8e39dfa1132aa52e118175cc347
sameerbutt28 <sameerofficial929@gmail.com>
2024-01-01 19:24:42 +0500

    Adding useParams in the Details.jsx

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,9 @@
-
+import { useParams } from "react-router-dom"
 const Details = () => {
+    const {id} = useParams(); //it gets the data through the browser router on the App.jsx
   return (
     <div>
-      <h1>Salam</h1>
+      <h1>{id}</h1>
     </div>
   )
 }
