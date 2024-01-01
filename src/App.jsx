b60a9f41ce0810eacdd1e54826271362c3454c94
sameerbutt28 <sameerofficial929@gmail.com>
2024-01-01 18:28:55 +0500

    Adding react router on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import  { createRoot }  from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
-
+import Details from "./Details";
   const App = () => {    
-  return (       
-    <div>     
+  return (  
+    <BrowserRouter >
+       
       <h1>Adopt Me!</h1>
-      <SearchParams /> 
-    </div>
+    <Routes>
+      
+<Route path="/details/:id" element={<Details />}  />
+<Route path="/" element={<SearchParams />}/>
+
+    </Routes>
+    </BrowserRouter>     
+    
   );
 };
 const container = document.getElementById("root");
