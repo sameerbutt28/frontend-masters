1424dfc9ab6650e2b3cef9233a463061a2564b7c
sameerbutt28 <sameerofficial929@gmail.com>
2024-01-01 18:55:36 +0500

    router started working

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
-import  { createRoot }  from "react-dom/client";
+import React from "react";
+import { createRoot } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
-  const App = () => {    
-  return (  
-    <BrowserRouter >
-       
-      <h1>Adopt Me!</h1>
-    <Routes>
-      
-<Route path="/details/:id" element={<Details />}  />
-<Route path="/" element={<SearchParams />}/>
 
-    </Routes>
-    </BrowserRouter>     
-    
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
+
 const container = document.getElementById("root");
-const root = createRoot(container); //old method 
+const root = createRoot(container);
 root.render(<App />);
