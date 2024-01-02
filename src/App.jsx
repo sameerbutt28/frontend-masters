eaf82924f844e8651477f71066cd018dbbea4734
sameerbutt28 <sameerofficial929@gmail.com>
2024-01-02 23:26:19 +0500

    adding a new componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
-
+ //react query cache is stored in memory 
+//  const QueryClient
 const App = () => {
   return (
 
