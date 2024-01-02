7ed8b847801f22718e6ced6d215a4f82ce919f51
sameerbutt28 <sameerofficial929@gmail.com>
2024-01-02 23:39:38 +0500

    adding clientQuery logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { createRoot } from "react-dom";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
- //react query cache is stored in memory 
-//  const QueryClient
+//react query cache is stored in memory
+const QueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity, //this logic is trying to say that once you frtch something do not fetch it again.
+      cacheTime: Infinity,
+    },
+  },
+});
 const App = () => {
-  return (    
-
-    <BrowserRouter>  
-    <header>
-    <Link to="/">Adopt Me!</Link> 
-    {/* header must be in the browser router */}
-    </header>
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+        {/* header must be in the browser router */}
+      </header>
       <div>
         <Routes>
           <Route path="/details/:id" element={<Details />} />
