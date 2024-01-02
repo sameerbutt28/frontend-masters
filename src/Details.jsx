d2540055d11144062c97ab309ef53a475cb953be
sameerbutt28 <sameerofficial929@gmail.com>
2024-01-03 00:51:28 +0500

    adding query client to details.js

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,6 +1,21 @@
 import { useParams } from "react-router-dom"
+import { QueryClient, useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchpet";
 const Details = () => {
     const {id} = useParams(); //it gets the data through the browser router on the App.jsx
+    const results = useQuery(["details", id], fetchPet)
+    // u cannot awiat in render function.
+    if(results.isLoading)
+    {
+      return(
+        <div className="loading-pane">
+          <h2 className="loader">
+spin
+          </h2>
+
+        </div>
+      )
+    }
   return (
     <div>
       <h1>{id}</h1>
